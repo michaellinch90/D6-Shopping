96136a4172c3a06e00a81c38ad53622142b215f8
MichaelLinch90 <michaellinch90@gmail.com>
2022-10-11 14:55:27 -0700

    add order-api set route path in server.js. built skeleton for order controller

---
+++ controllers/orders.js
@@ -0,0 +1,29 @@
+//const Order = require('../models/order')
+//const item = require('../models/item')
+
+//a cart is the unpaid items
+async function cart(req, res){
+
+}
+
+//add an item the the cart
+async function addToCart(req,res){
+
+}
+
+//updates an items qty in the cart
+async function setItemQtyInCart(req, res) {
+
+}
+
+//update the carts isPaid to true
+async function checkout(req,res){
+
+}
+
+module.exports = {
+    cart,
+    addToCart,
+    setItemQtyInCart,
+    checkout
+}
---
+++ src/utilities/orders.api.js
@@ -0,0 +1,26 @@
+import sendRequest from "./send-request-api";
+
+const BASE_URL = '/api/orders'
+
+//retireve unpaid item 
+export function getCart(){
+    return sendRequest(`${BASE_URL}/cart`)
+}
+
+export function assItemToCart(itemId){
+    //only sent item id for security reasons
+    //no pricing
+    return sendRequest(`{BASE_URL}/cart/items/${itemid}`, 'POST')
+}
+
+// update the item's qty in the cart
+//will add item if not currently in cart
+//sends info via data to payload instead of long url
+export function setItemInCart(itemId, newQty) {
+    return sendRequest(`${BASE_URL}/cart/qty`, 'PUT, { item, newQty }');
+}
+
+//updates the items to isPaid to true
+export function checkout(){
+    return sendRequest(`${BASE_URL}/cart/checkout`, 'POST')
+}
